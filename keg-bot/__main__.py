719c73c1597a7f86e8acf6715b9319d057ab736d
Jacob Smith <jacob.smith@slalom.com>
2019-10-14 12:48:38 -0600

    Updated __main__ to be entrypoint

--- keg-bot/__main__.py
+++ keg-bot/__main__.py
@@ -1,1 +1,11 @@
-print('keg-bot')
+import sys
+
+def main(args=None):
+  """Keg-Bot entry-point."""
+  if args is None:
+    args = sys.argv[1:]
+
+    print("Keg-Bot")
+
+if __name__ == "__main__":
+  main()
